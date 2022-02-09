ca61f5ed69d2a19b43c51b6ec3223b31d1659736
Kaisarion <beashuker@gmail.com>
2022-02-09 16:31:21 -0500

    Made handlers

---
+++ src/handlers/ready.js
@@ -0,0 +1,13 @@
+const chalk = require('chalk');
+
+module.exports = {
+    name: 'ready',
+    once: true,
+    execute: async(client) => {
+        console.log(
+            chalk.green.bold(`Ready to follow orders for AJ and mods at ${client.user.username}!`)
+        )
+
+
+    },
+};
